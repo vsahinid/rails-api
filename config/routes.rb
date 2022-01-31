89784c202f07fdbb5d97e4bbf90929e7afdb194d
Vasilios Sahinidis <vsahinid@alumni.cmu.edu>
2022-01-30 23:40:43 -0600

    Simple bookstore api using Rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :books
+        resources :subjects
+      end
+    end
+  end
 end
